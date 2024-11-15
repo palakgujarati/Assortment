6fb33850215d83b82e56885baa24b10cad9d8efd
palakgujarati <palakgujarati2106@gmail.com>
2024-11-15 16:21:22 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int a[10],i,size;
+    // declare the no of element 
+    
+    printf("enter the size:");
+    scanf("%d",&size);
+
+    // insert the value of element 
+    for(i=0; i<size; i++)
+    {
+        printf("a[%d]",i);
+        scanf("%d",&a[i]);
+    }
+    
+    //adding the condition to find the negative from the array
+    
+    for (i=0; i<size; i++)
+    {
+        if(a[i]<0)
+        {
+            printf("%d",a[i]);
+        }
+    }  
+}
